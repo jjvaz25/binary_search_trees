2d1b377f0e628ab592802aa7f2eb4a19574d50de
Jeffrey Vaz <jeff.j.vaz@gmail.com>
2019-11-19 10:45:05 -0500

    add #build_tree

--- build_tree.rb
+++ build_tree.rb
@@ -1,30 +1,43 @@
 class Node
   include Comparable
-  attr_accessor :value, :left_child, :right_child
-
-  def <=>(other)
-    value <=> other.value
-  end
+  attr_accessor :value, :left_child, :right_child, :parent
 
   def initialize(value)
     @value = value
     @left_child = nil
     @right_child = nil
+    @parent = nil
   end
 end
 
-nodeA = Node.new(10)
-nodeB = Node.new(15)
-nodeC = Node.new(2)
-nodeD = Node.new(10)
-
-p nodeA < nodeB #return true
-p nodeB < nodeA #return false
-p nodeA > nodeC #return true
-p nodeD.between?(nodeA, nodeB) #return true
-p nodeD > nodeB
 
 
-# def build_tree
-#   root = 
-# end
+def build_tree(array)
+  array = array.uniq
+  root = Node.new(array.shift)
+  while array.size != 0
+    current_node = Node.new(array.shift)
+    add_node(root, current_node)
+  end
+  root
+end
+
+def add_node(root, new_node)
+  if root.value > new_node.value #if the root is bigger than new_node, new_node is the left_child
+    if root.left_child == nil #if the root doesn't have a left child, make new_node the child
+      root.left_child = new_node
+      new_node.parent = root
+    else #if the root already has a left child, we need to recursively do the add_node
+      add_node(root.left_child, new_node)
+    end
+  else #root < new_node: root is less than new_node, so new_node is the right_child
+    if root.right_child == nil #if the root doesn't have a right child, make new_node the child
+      root.right_child = new_node
+      new_node.parent = root
+    else #if the root already has a right child, we need to recursively do the add_node
+      add_node(root.right_child, new_node)
+    end
+  end
+end
+
+p build_tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
