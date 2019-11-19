e7365f9aa82ce33e7ab4c2fb32bfa2b0a355668a
Jeffrey Vaz <jeff.j.vaz@gmail.com>
2019-11-19 07:16:02 -0500

    mixin Comparable module to Node class

--- build_tree.rb
+++ build_tree.rb
@@ -1,5 +1,10 @@
 class Node
+  include Comparable
   attr_accessor :value, :left_child, :right_child
+
+  def <=>(other)
+    value <=> other.value
+  end
 
   def initialize(value)
     @value = value
@@ -7,3 +12,19 @@ class Node
     @right_child = nil
   end
 end
+
+nodeA = Node.new(10)
+nodeB = Node.new(15)
+nodeC = Node.new(2)
+nodeD = Node.new(10)
+
+p nodeA < nodeB #return true
+p nodeB < nodeA #return false
+p nodeA > nodeC #return true
+p nodeD.between?(nodeA, nodeB) #return true
+p nodeD > nodeB
+
+
+# def build_tree
+#   root = 
+# end
