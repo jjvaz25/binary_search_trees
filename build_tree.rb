d01a0cc9c7130ac916b56292d6f2ea00bbbe6f70
Jeffrey Vaz <jeff.j.vaz@gmail.com>
2019-11-19 11:03:42 -0500

    add #to_s to visualize Tree class

--- build_tree.rb
+++ build_tree.rb
@@ -1,3 +1,4 @@
+
 class Node
   include Comparable
   attr_accessor :value, :left_child, :right_child, :parent
@@ -11,33 +12,59 @@ class Node
 end
 
 
+class Tree 
+  attr_accessor :array, :root
 
-def build_tree(array)
-  array = array.uniq
-  root = Node.new(array.shift)
-  while array.size != 0
-    current_node = Node.new(array.shift)
-    add_node(root, current_node)
+  def initialize (array)
+    @array = array.uniq
+    @root = nil
   end
-  root
-end
 
-def add_node(root, new_node)
-  if root.value > new_node.value #if the root is bigger than new_node, new_node is the left_child
-    if root.left_child == nil #if the root doesn't have a left child, make new_node the child
-      root.left_child = new_node
-      new_node.parent = root
-    else #if the root already has a left child, we need to recursively do the add_node
-      add_node(root.left_child, new_node)
+  def to_s(current = @root, indent = "   ")
+    if current == @root
+      puts "ROOT: #{@root.value}"
     end
-  else #root < new_node: root is less than new_node, so new_node is the right_child
-    if root.right_child == nil #if the root doesn't have a right child, make new_node the child
-      root.right_child = new_node
-      new_node.parent = root
-    else #if the root already has a right child, we need to recursively do the add_node
-      add_node(root.right_child, new_node)
+    indent += "  "
+    if current.left_child != nil
+      puts "#{indent}L: #{current.left_child.value}"
+      to_s(current.left_child, indent)
+    end
+    if current.right_child != nil
+      puts "#{indent}R: #{current.right_child.value}"
+      to_s(current.right_child, indent)
     end
   end
+
+  def build_tree
+    array = @array
+    @root = Node.new(array.shift)
+    while array.size != 0
+      current_node = Node.new(array.shift)
+      add_node(@root, current_node)
+    end
+    @root
+  end
+  
+  def add_node(root, new_node)
+    if root.value > new_node.value 
+      if root.left_child == nil 
+        root.left_child = new_node
+        new_node.parent = root
+      else 
+        add_node(root.left_child, new_node)
+      end
+    else 
+      if root.right_child == nil 
+        root.right_child = new_node
+        new_node.parent = root
+      else 
+        add_node(root.right_child, new_node)
+      end
+    end
+  end
 end
 
-p build_tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+tree.build_tree
+tree.to_s
+
