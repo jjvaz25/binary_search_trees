921a41eac6763b2e678caec68eefa5aaddbeea6d
Jeffrey Vaz <jeff.j.vaz@gmail.com>
2019-11-19 14:41:56 -0500

    add #insert to Tree class

--- build_tree.rb
+++ build_tree.rb
@@ -1,6 +1,5 @@
 
 class Node
-  include Comparable
   attr_accessor :value, :left_child, :right_child, :parent
 
   def initialize(value)
@@ -9,6 +8,7 @@ class Node
     @right_child = nil
     @parent = nil
   end
+
 end
 
 
@@ -22,7 +22,7 @@ class Tree
 
   def to_s(current = @root, indent = "   ")
     if current == @root
-      puts "ROOT: #{@root.value}"
+      puts "\n\nROOT: #{@root.value}"
     end
     indent += "  "
     if current.left_child != nil
@@ -75,17 +75,40 @@ class Tree
       root = root.right_child
       find(value, root)
     end
+  end
 
+  def insert(value, root = @root)
+    p node_to_insert = Node.new(value)
+    add_node(@root, node_to_insert)
+    # if root == nil #if the tree is empty insert this node as the root
+    #   p "the root is nil"
+    #   p root = node_to_insert
+    # elsif node_to_insert.value < root.value #look to left children
+    #   puts "the desired inserted value #{node_to_insert.value} is less than root value"
+    #   new_root = root.left_child
+    #   insert(value, new_root)
+    # else #node_to_insert.value > root.valye look to right children
+    #   root = root.right_child
+    #   insert(value, root)
+    # end
   end
 
 end #end class
 
-tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-tree.build_tree
-tree.to_s
-p tree.root
-puts ""
-#p tree.find(1) #correct return!
-p tree.find(3)
+#unique_array = [1, 7, 4, 23, 8, 9, 3, 5, 67, 6345, 324]
 
+tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+p tree.build_tree
+puts "______________________"
+tree.to_s
+puts ""
+#print "searching for value 200 (expect nil): " 
+#p tree.find(200) #correct return
+#p tree.find(1) #correct return
+#p tree.find(3) #correct return
+#p tree.insert(10, nil) #correct return
+tree.insert(0) #correct return, left child of root/1
+tree.insert(20) #correct return, right child of 9
+tree.to_s
+puts "\n\n_______AFTER INSERTS______________\n\n"
 
